6a18b2d56c7955938685f8b2ba6d540e80e97187
Brandon <brandon.ballentine@stonybrook.edu>
2018-10-11 05:12:30 -0400

    started test driving Interface class

--- src/Interface.h
+++ src/Interface.h
@@ -1,6 +1,187 @@
 #include <iostream>
+#include <cassert>
+#include <vector>
+#include <list>
+
+
+class Point
+{
+    private:
+     
+        int dim {3}; 
+        double elem[3];
+
+    public:
+    
+        Point() = default;
+        Point(const Point&) = default;
+        Point& operator=(const Point&) = default;
+        Point(Point&&) = default;
+        Point& operator=(Point&&) = default;
+        ~Point() = default;
+
+        Point(double x, double y)
+            : dim{2}
+        {
+            elem[0] = x;
+            elem[1] = y;
+            elem[2] = 0;
+        }
+
+        Point(double x, double y, double z)
+        {
+            elem[0] = x;
+            elem[1] = y;
+            elem[2] = z;
+        }
+
+        const double& operator[](const std::size_t i) const
+        {
+            assert(i >= 0 && i < dim);
+            return elem[i];
+        }
+
+        double& operator[](const std::size_t i)
+        {
+            const_cast<double&>(
+                    static_cast<const Point&>(*this)[i]);
+        }
+};
+
+class Bond
+{
+    private:
+
+        Point p1;
+        Point p2;
+
+    public:
+    
+        Bond() = default;
+        Bond(const Bond&) = default;
+        Bond& operator=(const Bond&) = default;
+        Bond(Bond&&) = default;
+        Bond& operator=(Bond&&) = default;
+        ~Bond() = default;
+
+        Bond(const Point& p, const Point& q)
+            : p1{p}, p2{q}
+        {}
+
+        const Point& operator[](const std::size_t i) const
+        {
+            assert(i == 0 || i == 1);
+            return i == 0 ? p1 : p2;
+        }
+
+        Point& operator[](const std::size_t i)
+        {
+            const_cast<Point&>(
+                    static_cast<const Bond&>(*this)[i]);
+        }
+};
+
+
+class Curve
+{
+    private:
+
+        std::list<Bond> segments;
+
+    public:
+
+        Curve() = default;
+        Curve(const Curve&) = default;
+        Curve& operator=(const Curve&) = default;
+        Curve(Curve&&) = default;
+        Curve& operator=(Curve&&) = default;
+        ~Curve() = default;
+
+};
+
 
 class Interface
 {
+    protected:
+
+        std::vector<std::vector<Point> > points;
+        std::vector<Curve> curves;
+
+    public:
+
+        Interface() = default;
+        ~Interface() = default;
+
+        Interface(const Interface&) = delete;
+        Interface& operator=(const Interface&) = delete;
+        Interface(Interface&&) = delete;
+        Interface& operator=(Interface&&) = delete;
+
+        virtual const int getDim() const = 0;
+
+        /*
+        const Point& getPoint(const std::size_t i) const
+        {
+            assert(i < points.size());
+            return points[i];
+        }
+
+        const Curve& getCurve(const std::size_t i) const
+        {
+            assert(i < curves.size());
+            return curves[i];
+        }
+        */
+};
+
+class Interface2d : public Interface
+{
+    private:
+
+        void insertClosedCurve(const std::vector<Point>& pts)
+        {
+
+        }
+
+    public:
+
+        Interface2d() = default;
+        ~Interface2d() = default;
+
+        Interface2d(const Interface2d&) = delete;
+        Interface2d& operator=(const Interface2d&) = delete;
+        Interface2d(Interface2d&&) = delete;
+        Interface2d& operator=(Interface2d&&) = delete;
+
+        const int getDim() const override {return 2;}
+
+        void insertClosedCurvePoints(const std::vector<Point>& pts)
+        {
+            //points.assign(pts.begin(),pts.end());
+            insertClosedCurved(pts);
+        }
 
 };
+
+
+class Interface3d : public Interface
+{
+    public:
+
+        Interface3d() = default;
+        ~Interface3d() = default;
+
+        Interface3d(const Interface3d&) = delete;
+        Interface3d& operator=(const Interface3d&) = delete;
+        Interface3d(Interface3d&&) = delete;
+        Interface3d& operator=(Interface3d&&) = delete;
+
+        const int getDim() const override {return 3;}
+
+
+};
+
+
+
+
+
--- tests/InterfaceTests.cpp
+++ tests/InterfaceTests.cpp
@@ -2,7 +2,54 @@
 #include "gtest/gtest.h"
 
 
-TEST(InterfaceTests, AddPointToInterface)
+TEST(InterfaceTests, InsertCurveInterface2d)
 {
-    Interface intfc;
+    std::vector<Point> pts;
+    pts.push_back(Point(0,0));
+    pts.push_back(Point(1,0));
+    pts.push_back(Point(1,1));
+    
+    Interface2d intfc2d;
+    intfc2d.insertClosedCurvePoints(pts);
+
+    /*
+    ASSERT_DOUBLE_EQ(intfc2d.getPoint(0)[0], 0);
+    ASSERT_DOUBLE_EQ(intfc2d.getPoint(0)[1], 0);
+
+    ASSERT_DOUBLE_EQ(intfc2d.getPoint(1)[0], 1);
+    ASSERT_DOUBLE_EQ(intfc2d.getPoint(1)[1], 0);
+    
+    ASSERT_DOUBLE_EQ(intfc2d.getPoint(2)[0], 1);
+    ASSERT_DOUBLE_EQ(intfc2d.getPoint(2)[1], 1);
+    */
+
+    //ASSERT_EQ(intfc2d.getCurve(0).begin(),
+      //      intfc2d.getCurve(0).end()->next);
 }
+
+/*
+TEST(InterfaceTests, Add3dPointsToInterface)
+{
+    std::vector<Point> pts;
+    pts.push_back(Point(0,0,0));
+    pts.push_back(Point(1,0,0));
+    pts.push_back(Point(1,1,1));
+    
+    Interface intfc;
+    intfc.addPoints(pts);
+
+    ASSERT_DOUBLE_EQ(intfc.getPoint(0)[0], 0);
+    ASSERT_DOUBLE_EQ(intfc.getPoint(0)[1], 0);
+    ASSERT_DOUBLE_EQ(intfc.getPoint(0)[2], 0);
+
+    ASSERT_DOUBLE_EQ(intfc.getPoint(1)[0], 1);
+    ASSERT_DOUBLE_EQ(intfc.getPoint(1)[1], 0);
+    ASSERT_DOUBLE_EQ(intfc.getPoint(1)[2], 0);
+    
+    ASSERT_DOUBLE_EQ(intfc.getPoint(2)[0], 1);
+    ASSERT_DOUBLE_EQ(intfc.getPoint(2)[1], 1);
+    ASSERT_DOUBLE_EQ(intfc.getPoint(2)[2], 1);
+}
+*/
+
+
